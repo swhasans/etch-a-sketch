aa997ede383aa068d368922e24cba6bdb0185559
swhasans <SS3858@live.mdx.ac.uk>
2023-04-27 23:23:01 -0400

    Add basic flex container

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const container = document.querySelector("#flex-container");
 container.setAttribute(
   "style",
-  "background-color:white; border:3px solid black; height:850px; width:850px; display: flex"
+  "background-color:white; border:3px solid black; height:1000px; width:960px; display: flex; justify-content: flex-start; align-items: stretch; margin: auto; flex-wrap: wrap"
 );
 console.log(container);
 
 const userSelection = document.querySelector("#user-selection-squares");
 console.log(userSelection);
 
-function clearBox(elementID)
-{
-    document.getElementById(elementID).innerHTML = "";
+function clearBox() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
 
 const appendNumberofDivsSelectedByUser = function (e) {
-    while(container.firstChild){
-        container.removeChild(container.firstChild);
-    }
-    
-    console.log(e.target.value);
-    for (let i = 0; i < Number(e.target.value); i++) {
-      const newDiv = (document.createElement("div"));
-      newDiv.setAttribute(
-          "style",
-          "background-color:peachpuff; border:1px solid brown; height:100px; width:100px"
-        );    
-      container.appendChild(newDiv); 
-    }
-  };
+  clearBox();
 
-  userSelection.addEventListener('input', appendNumberofDivsSelectedByUser);
+  console.log(e.target.value);
+  for (let i = 0; i < Number(e.target.value); i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute(
+      "style",
+      "background-color:peachpuff; border:1px solid brown; height:100px; width:100px"
+    );
+    container.appendChild(newDiv);
+  }
+};
 
-
+userSelection.addEventListener("input", appendNumberofDivsSelectedByUser);
