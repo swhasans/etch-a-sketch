c0afb3e74bcf6121b670caba24ec5dc8f1445f3e
swhasans <SS3858@live.mdx.ac.uk>
2023-04-27 22:54:10 -0400

    Add basic grid without wrap

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const container = document.querySelector("#flex-container")
+const container = document.querySelector("#flex-container");
+container.setAttribute(
+  "style",
+  "background-color:white; border:3px solid black; height:850px; width:850px; display: flex"
+);
 console.log(container);
 
-const user_selection = function(x){
-    for(let i = 0; i < x; i++)
-    {
+const userSelection = document.querySelector("#user-selection-squares");
+console.log(userSelection);
 
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+const appendNumberofDivsSelectedByUser = function (e) {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
-};
+    
+    console.log(e.target.value);
+    for (let i = 0; i < Number(e.target.value); i++) {
+      const newDiv = (document.createElement("div"));
+      newDiv.setAttribute(
+          "style",
+          "background-color:peachpuff; border:1px solid brown; height:100px; width:100px"
+        );    
+      container.appendChild(newDiv); 
+    }
+  };
+
+  userSelection.addEventListener('input', appendNumberofDivsSelectedByUser);
+
+
