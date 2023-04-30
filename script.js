d0c4c96bbade135dc61c5b63038aa62fddc6442b
swhasans <SS3858@live.mdx.ac.uk>
2023-04-30 15:54:29 -0400

    Add change color of div to black on event 'mousedown', done by listening to event attached to the container

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".flex-container");
 const userSelection = document.querySelector("#user-selection-squares");
 let squaresPerSide = 16;
 
-function clearBox() {
+function clearBox() { 
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
@@ -10,20 +10,26 @@ function clearBox() {
 
 function createGrid() {
   container.style.setProperty('--squares-per-side', squaresPerSide);
-  clearBox();
+  clearBox();   
 
   for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add('newDiv');
     container.appendChild(newDiv);
   }
-};
+}
 
 function numOfDivsInputtedByUser (event){
     squaresPerSide = Number(event.target.value);
     createGrid();
 }
 
+function changeColourOfDiv(event){
+    event.target.style.setProperty('background-color', "black")
+    console.log(event.target);
+} 
+
 userSelection.addEventListener("input", numOfDivsInputtedByUser);
+container.addEventListener("mousedown", changeColourOfDiv);
 
 createGrid();
