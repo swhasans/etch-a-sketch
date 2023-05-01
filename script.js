c5f98530a02e42ed35efeeb92160e52c981a45cc
swhasans <SS3858@live.mdx.ac.uk>
2023-04-30 21:22:35 -0400

    'Color Black' and 'Color Random' button work. Bug: By default no color is selected.

--- script.js
+++ script.js
@@ -5,49 +5,58 @@ const userSelectRandom = document.querySelector("#user-color-squares-random");
 const userSelectClear = document.querySelector("#user-clear-squares");
 let squaresPerSide = 16;
 
-function clearBox() { 
+function clearBox() {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 }
 
-function eraseBox(){
-    clearBox();
-    createGrid();
+function eraseBox() {
+  clearBox();
+  createGrid();
 }
 
 function createGrid() {
-  container.style.setProperty('--squares-per-side', squaresPerSide);
-  clearBox();   
+  container.style.setProperty("--squares-per-side", squaresPerSide);
+  clearBox();
 
   for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add('newDiv');
+    newDiv.classList.add("newDiv");
     container.appendChild(newDiv);
   }
 }
 
-function numOfDivsInputtedByUser (event){
-    squaresPerSide = Number(event.target.value);
-    createGrid();
+function numOfDivsInputtedByUser(event) {
+  squaresPerSide = Number(event.target.value);
+  createGrid();
 }
 
-function changeColourOfDivtoBlack(event){
-    event.target.style.setProperty('background-color', "black")
-} 
+function changeColourOfDivtoBlack(event) {
+  event.target.style.setProperty("background-color", "black");
+}
 
-function changeColourOfDivtoRandom(event){
-    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-    console.log("randomColor : " + randomColor);
-    event.target.style.setProperty('background-color', randomColor)
-} 
+function changeColourOfDivtoRandom(event) {
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  console.log("randomColor : " + randomColor);
+  event.target.style.setProperty("background-color", randomColor);
+}
 
-userInputNumOfDiv.addEventListener("input", numOfDivsInputtedByUser);
-container.addEventListener("mousedown", changeColourOfDivtoBlack);
+if (userSelectBlack) {
+  userSelectBlack.addEventListener("click", function () {
+    container.removeEventListener("mousedown", changeColourOfDivtoRandom);
+    container.addEventListener("mousedown", changeColourOfDivtoBlack);
+  });
+}
 
-container.addEventListener("mousedown", changeColourOfDivtoRandom);
-
+if (userSelectRandom) {
+  userSelectRandom.addEventListener("click", function () {
+    container.removeEventListener("mousedown", changeColourOfDivtoBlack);
+    container.addEventListener("mousedown", changeColourOfDivtoRandom);
+  });
+}
 
 userSelectClear.addEventListener("click", eraseBox);
 
 createGrid();
+
